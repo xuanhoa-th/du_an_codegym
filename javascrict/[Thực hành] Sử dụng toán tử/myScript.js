7bebfd245be14d82fd8b27f52d2e5a1e0963a314
hkbike <xuanhoa93th@gmail.com>
2020-03-03 16:38:13 +0700

    kaka

---
+++ javascrict/[Thực hành] Sử dụng toán tử/myScript.js
@@ -0,0 +1,10 @@
+		let inputWidth;
+		let inputHeight;
+
+		inputWidth = prompt("Enter the width");
+		inputHeight = prompt("Enter the height");
+
+		let width = parseInt(inputWidth);
+		let height = parseInt(inputHeight);
+		let arina = width*height;
+		document.write("the arina is:" + arina);
