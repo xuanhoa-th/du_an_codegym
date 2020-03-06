2f63bd705fd08099c533af04744690ae6c1d9b00
hkbike <xuanhoa93th@gmail.com>
2020-03-06 13:52:48 +0700

    6-3

---
+++ javascrict/[Thực hành] Luyện tập với cấu trúc if...else...if/app.js
@@ -0,0 +1,32 @@
+// let userName = prompt("Who's there?", '');
+// if (userName == 'Admin') {
+// 	let pass = prompt('Password?','');
+// 	if (pass == 'TheMaster') {
+// 		alert('Welcome');
+// 	} else if (pass == null) {
+// 		alert('Canceled.');
+// 	} else {
+// 		alert('Wrong password');
+// 	}
+// } else if (userName == null) {
+// 	alert('Canceled');
+// } else {
+// 	alert("I don't know you");
+// }
+
+let userName = prompt("who's there?",'');
+if (userName == 'admin') {
+	alert("welcome");
+} else if (userName == null){
+	alert("canceled");
+} else {
+	alert(" i dont know");
+}
+let pass = prompt("password",'');
+if (pass == 'theMaster') {
+	alert("wellcom");
+} else if (pass == null) {
+	alert('canled');
+} else {
+	alert("wrong password");
+}
---
+++ javascrict/[Thực hành] Luyện tập với toán tử ba ngôi/1.js
@@ -0,0 +1,14 @@
+// login = prompt("moi ban nhap", '');
+// let message = (login == 'Employee')? 'Hello':
+// (login == 'Director') ? 'Greetings' :
+// (login == '') ? 'No login' : '';
+// alert(message);
+
+
+login =prompt("moi ban dang nhap", '');
+
+
+let  message =( login == 'Employee') ? 'Hello' :
+( login == 'Director' )? 'Greetings' :
+(login == '') ? 'No login' : '';
+alert(message);
